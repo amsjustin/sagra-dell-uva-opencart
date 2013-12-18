809ab5ead4537186fe1d24b7ebf7e98dec42fe36
Martijn Oppedijk <martijn@thepagecreators.com>
2013-12-18 12:54:09 +0100

    Add wine en header change

---
+++ catalog/language/english/account/addwine.php
@@ -0,0 +1,8 @@
+<?php
+// Heading 
+$_['heading_title']                = 'Wijn toevoegen';
+
+// Text
+$_['text_addwine']                 = 'Wijn toevoegen';
+$_['text_account ']                 = 'Profiel';
+?>
